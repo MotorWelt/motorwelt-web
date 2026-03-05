2f01749a8cd2e5025e0ef3b78c7df1d5f381c79c
Gabriel Rodríguez Gámez <gabriel@motorwelt.mx>
2026-03-05 17:52:46 -0600

    add pages folder

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:"sans-serif"}}>
+      <h1>MotorWelt</h1>
+      <p>El sitio está en construcción.</p>
+    </div>
+  );
+}
