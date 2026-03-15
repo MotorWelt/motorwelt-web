32443c532c7327f412aa7e34291dee239e7824f4
Gabriel Rodríguez Gámez <gabriel@motorwelt.mx>
2026-03-14 23:51:27 -0600

    fix i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,28 @@
+// next-i18next.config.js
+const path = require("path");
+
 module.exports = {
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    localeDetection: false,
   },
+
+  localePath:
+    typeof window === "undefined"
+      ? path.resolve("./public/locales")
+      : "/locales",
+
+  defaultNS: "home",
+  ns: ["home"],
+
+  fallbackLng: {
+    default: ["es"],
+    en: ["en"],
+    es: ["es"],
+  },
+
+  nonExplicitSupportedLngs: true,
+  reloadOnPrerender: process.env.NODE_ENV === "development",
+  debug: process.env.NODE_ENV === "development",
 };
